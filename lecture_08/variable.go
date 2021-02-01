95d1a38a43589baa5a848f984d2f80b8d08705b8
TanvirFahimBD <tanvirfahim6939@gmail.com>
2021-02-01 10:12:55 +0600

    variable learned

---
+++ lecture_08/variable.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+
+/*
+var name string = "Fahim"
+var name2, city string = "Tanvir", "Khulna"
+fmt.Println(name, city, name2)
+*/
+
+var c rune = 'A'
+var age int = 10
+var result float32 = 23.64
+
+fmt.Println(c, age, result)
+
+}
