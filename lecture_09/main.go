9fb7c4eece5732ab9d25059b5132c4d45d5e758f
TanvirFahimBD <tanvirfahim6939@gmail.com>
2021-02-07 12:28:40 +0600

    simple program added

--- lecture_09/main.go
+++ lecture_09/main.go
@@ -4,27 +4,10 @@ import "fmt"
 
 func main() {
 
-	// var name string
-	// var age int
-	// var result float32
-	// var chr rune
-	// var isFound bool
-
-	// fmt.Println(name)
-	// fmt.Println(age)
-	// fmt.Println(result)
-	// fmt.Println(chr)
-
-	// fmt.Println(isFound)
-	// fmt.Println(true && true)
-	// fmt.Println(true && false)
-	// fmt.Println(true || true)
-	// fmt.Println(true || false)
-	// fmt.Println(!true)
-
-	var chr rune
-	chr = 'B'
-	fmt.Println(chr)
-	fmt.Printf("%c ", chr)
-
+ fmt.Println("Enter Your name and age: ")
+ var name string
+ var age int
+ fmt.Scanf("%s %d", &name, &age)
+ fmt.Printf("Your name %s and age is %d", name, age)
+	
 }
