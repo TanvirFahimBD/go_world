05b9138c4e9a32828cad391c66224b8f83c4bb2a
TanvirFahimBD <tanvirfahim6939@gmail.com>
2021-02-08 11:48:34 +0600

    map code added

--- lecture_10/main.go
+++ lecture_10/main.go
@@ -1,53 +1,30 @@
 package main
 
-// func main() {
-
-// 	//  var students [3]string
-
-// 	//  students[0] = "Tanvir"
-// 	//  students[1] = "Hossain"
-// 	//  students[2] = "Fahim"
-
-// 	//  x := students[0:2]
-// 	//  fmt.Println(x)
-
-// 	// var students [3]string
-
-// 	// x := make([]string, 0)
-// 	// fmt.Println(x)
-// 	var students [3]string
-// 	var fruits []string
-// 	fruits = append(fruits, "Apple", "Mango")
-
-// 	fmt.Println(fruits, len(fruits))
-
-// 	fmt.Printf("%T\n", fruits)
-// 	fmt.Printf("%T", students)
-
-// }
-
-//Identify slice code
-
-package main
-
 import "fmt"
-import "reflect"
 
 func main() {
 
- var students [3]string
- var fruits []string
- fruits = append(fruits, "Apple","Mango")
- 
- fmt.Println(fruits, len(fruits))
- 
- fmt.Printf("%T\n", fruits) 
- fmt.Printf("%T\n", students)
+	// x := make(map[string]string)
 
- a:= reflect.TypeOf(students).Kind().String()
- b:= reflect.TypeOf(fruits).Kind().String()
- fmt.Println(a)
- fmt.Println(b)
+	// x["name"] = "Tanvir"
+	// x["age"] = "22"
+
+	// fmt.Println(x)
+
+	// x := make(map[string]string)
+
+	// x["name"] = "Tanvir"
+	// x["age"] = "22"
+
+	// fmt.Println(x["name"])
+
+	x := make(map[string]string)
+
+	x["name"] = "Tanvir"
+	x["age"] = "22"
+
+	delete(x, "age")
+
+	fmt.Println(x)
 
 }
-
