29012a7fb5df18baeeb0db793fa8e250e9f0afc2
TanvirFahimBD <tanvirfahim6939@gmail.com>
2021-02-08 11:45:13 +0600

    array code added

--- lecture_10/main.go
+++ lecture_10/main.go
@@ -14,3 +14,29 @@ func main() {
  fmt.Println(x)
 
 }
+package main
+
+import "fmt"
+
+func main() {
+
+//  var students [3]string
+//  students[0] = "Tanvir"
+//  students[1] = "Hossain"
+//  students[2] = "Fahim"
+
+//  fmt.Println(students)
+//  fmt.Println(len(students))
+//  fmt.Println(students[0])
+//  fmt.Println(students[1])
+//  fmt.Println(students[2])
+
+
+// students := [3]string{"Tanvir","Hossain","Fahim"}
+// fmt.Println(students)
+
+students := [...]string{"Tanvir","Hossain","Fahim","learning","Golang"}
+ fmt.Println(students)
+
+
+}
